7e80cc2a02b07e90e9fb559d0875e5540e7a0895
tsilicanidev <tsilicani@gmail.com>
2025-05-19 15:01:36 -0300

    up

--- src/pages/NotaFiscalVisualizacao.tsx
+++ src/pages/NotaFiscalVisualizacao.tsx
@@ -15,4 +15,18 @@ const NotaFiscalVisualizacao: React.FC = () => {
   );
 };
 
+import { VisualizarDanfeButton } from '@/components/VisualizarDanfeButton';
+
+// Exemplo de XML fixo para teste (substitua por XML real se necessário)
+const exemploXml = `<NFe xmlns=\"http://www.portalfiscal.inf.br/nfe\"><infNFe versao=\"4.00\" Id=\"NFe00000000000000000000000000000000000000000000\"></infNFe></NFe>`;
+
+function NotaFiscalVisualizacao() {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold mb-4">Visualização da Nota Fiscal</h1>
+      <VisualizarDanfeButton xml={exemploXml} />
+    </div>
+  );
+}
+
 export default NotaFiscalVisualizacao;
