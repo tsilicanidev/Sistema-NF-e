40a38bf6029ec162d55de618e5e0a0a31e78e273
tsilicanidev <tsilicani@gmail.com>
2025-05-21 17:11:55 -0300

    Update nfeUtils.ts

--- src/utils/nfeUtils.ts
+++ src/utils/nfeUtils.ts
@@ -133,7 +133,6 @@ export function assinarXml(xml: string, certificateData: CertificateData): strin
 
     // Define os algoritmos ANTES de adicionar a referência
     sig.signatureAlgorithm = "http://www.w3.org/2000/09/xmldsig#rsa-sha1";
-    sig.digestAlgorithm = "http://www.w3.org/2000/09/xmldsig#sha1";
     sig.canonicalizationAlgorithm = "http://www.w3.org/TR/2001/REC-xml-c14n-20010315";
     sig.signingKey = privateKey;
 
