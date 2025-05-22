9bb8b4496f31a3c180b0dac31132356ba42a89c3
tsilicanidev <tsilicani@gmail.com>
2025-05-21 23:48:11 -0300

    Update nfeUtils.ts

--- src/utils/nfeUtils.ts
+++ src/utils/nfeUtils.ts
@@ -80,22 +80,21 @@ const sig = new SignedXml();
 
 // Ordem correta das propriedades
 sig.signatureAlgorithm = 'http://www.w3.org/2000/09/xmldsig#rsa-sha1';
+sig.digestAlgorithm = 'http://www.w3.org/2000/09/xmldsig#sha1'; // <-- definindo aqui!
 sig.canonicalizationAlgorithm = 'http://www.w3.org/TR/2001/REC-xml-c14n-20010315';
 sig.signingKey = privateKey;
 
-// Informações do certificado
 sig.keyInfoProvider = {
   getKeyInfo: () => `<X509Data><X509Certificate>${certificate}</X509Certificate></X509Data>`,
 };
 
-// ✅ digestAlgorithm deve estar aqui — como terceiro argumento:
 sig.addReference(
   `//*[local-name(.)='infNFe' and @Id='${id}']`,
   [
     'http://www.w3.org/2000/09/xmldsig#enveloped-signature',
     'http://www.w3.org/TR/2001/REC-xml-c14n-20010315'
-  ],
-  'http://www.w3.org/2000/09/xmldsig#sha1' // <-- CORRETO AQUI
+  ]
+  // ❌ remova o digestAlgorithm daqui — isso só funciona em versões futuras
 );
 
 // Gera a assinatura
