8755e6cc47789c39b673f56362b27b2ce9ada96f
tsilicanidev <tsilicani@gmail.com>
2025-05-23 11:07:00 -0300

    Migrate project to JavaScript and add SEFAZ integration

---
+++ vite.config.js
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    nodePolyfills({
+      globals: {
+        Buffer: true,
+        global: true,
+        process: true,
+      },
+      protocolImports: true,
+    }),
+  ],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+  build: {
+    rollupOptions: {
+      external: ['libxmljs2'],
+    },
+  },
+  server: {
+    proxy: {
+      '/sefaz': {
+        target: 'https://nfe.sefaz.sp.gov.br',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/sefaz/, ''),
+      },
+    },
+  },
+});
